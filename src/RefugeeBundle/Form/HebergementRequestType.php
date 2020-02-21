61618ac778ec4ef320cdc3d34ebf0dfe439b6d3f
Mohamed Nasri <muhammed.al.nasri@gmail.com>
2020-02-21 15:37:48 +0100

    search

--- src/RefugeeBundle/Form/HebergementRequestType.php
+++ src/RefugeeBundle/Form/HebergementRequestType.php
@@ -3,6 +3,9 @@
 namespace RefugeeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +17,23 @@ class HebergementRequestType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('isAnonymous', CheckboxType::class)
+            ->add('name')
+            ->add('telephone')
             ->add('description')
             ->add('region')
-            ->add('state')
-            ->add('user');
+            ->add('nativeCountry')
+            ->add('arrivalDate', DateType::class)
+            ->add('passportNumber')
+            ->add('civilStatus', ChoiceType::class,
+                [
+                    'label' => 'Etat Civil: ',
+                    'choices' => [
+                        'Marié(e)' => 0,
+                        'Celibatair(e)' => 1
+                    ]
+                ])
+            ->add('childrenNumber');
     }/**
      * {@inheritdoc}
      */
